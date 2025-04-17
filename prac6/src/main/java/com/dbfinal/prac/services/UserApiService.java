75f3452d70624903725725ceda7b285844b793d2
BekzatAitbekov <bekzat_aitbekov@epam.com>
2025-04-17 16:24:09 +0500

    updated UserController

--- prac6/src/main/java/com/dbfinal/prac/services/UserApiService.java
+++ prac6/src/main/java/com/dbfinal/prac/services/UserApiService.java
@@ -1,20 +1,21 @@
 package com.dbfinal.prac.services;
 
+import com.dbfinal.prac.dto.UserShowDto;
 import com.dbfinal.prac.models.User;
 
 import java.util.List;
 
 public interface UserApiService {
     boolean existsByEmail(String email);
-    List<User> getAllUsers();
+    List<UserShowDto> getAllUsers();
 
-    User getUserById(String id);
+    UserShowDto getUserById(String id);
 
-    User getUserByName(String name);
-
-    User addUser(User user);
+    UserShowDto getUserByName(String name);
 
     User update(String id, User updatedUser);
 
     void delete(String id);
+
+    UserShowDto convertToDto(User user);
 }
